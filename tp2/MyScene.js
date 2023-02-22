5b56e903d3cf90d11107bca3e7e1eef3eff940c4
Marcelo <marceloguarniero@gmail.com>
2023-02-22 17:38:21 +0000

    TP2, Ex 1, Step 3

--- tp2/MyScene.js
+++ tp2/MyScene.js
@@ -1,9 +1,5 @@
 import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
-import { MyDiamond } from "./MyDiamond.js";
-import { MyParallelogram } from "./MyParallelogram.js";
-import { MyTriangle } from "./MyTriangle.js";
-import { MyTriangleBig } from "./MyTriangleBig.js";
-import { MyTriangleSmall } from "./MyTriangleSmall.js";
+import { MyTangram } from "./MyTangram.js";
 
 /**
  * MyScene
@@ -29,13 +25,7 @@ export class MyScene extends CGFscene {
 
     //Initialize scene objects
     this.axis = new CGFaxis(this);
-    this.diamond = new MyDiamond(this);
-    this.blueTriangle = new MyTriangleBig(this);
-    this.orangeTriangle = new MyTriangleBig(this);
-    this.parallelogram = new MyParallelogram(this);
-    this.pinkTriangle = new MyTriangle(this);
-    this.purpleTriangle = new MyTriangleSmall(this);
-    this.redTriangle = new MyTriangleSmall(this);
+    this.tangram = new MyTangram(this);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
@@ -100,92 +90,7 @@ export class MyScene extends CGFscene {
     this.multMatrix(sca);
 
     // ---- BEGIN Primitive drawing section
-
-    // Drawing the head (diamond)
-    this.pushMatrix();
-
-    var matrixRotate = [
-      Math.cos(Math.PI/4), Math.sin(Math.PI/4), 0, 0,
-      -Math.sin(Math.PI/4), Math.cos(Math.PI/4), 0, 0,
-      0, 0, 1, 0,
-      0, 0, 0, 1
-    ];
-    var matrixTranslate = [
-      1, 0, 0, 0,
-      0, 1, 0, 0,
-      0, 0, 1, 0,
-      0, 3*Math.sqrt(2), 0, 1
-    ];
-
-    this.multMatrix(matrixTranslate);
-    this.multMatrix(matrixRotate);
-    this.diamond.display();
-
-    this.popMatrix();
-
-    // Drawing the blue triangle
-    this.pushMatrix();
-
-    this.translate(0, Math.sqrt(2)/2, 0);
-    this.rotate(5*Math.PI/4, 0, 0, 1);
-    this.translate(0, -2, 0);
-
-    this.blueTriangle.display();
-
-    this.popMatrix();
-
-    // Drawing the orange triangle
-    this.pushMatrix();
-
-    this.translate(0, 3*Math.sqrt(2)-Math.sqrt(2)/2, 0);
-    this.rotate(-Math.PI/2, 0, 0, 1);
-    this.translate(2, 0, 0);
-
-    this.orangeTriangle.display();
-
-    this.popMatrix();
-
-    // Drawing the parallelogram
-    this.pushMatrix();
-
-    this.translate(0, 0.5, 0);
-    this.rotate(-Math.PI/2, 0, 0, 1);
-    this.scale(1, -1, 1);
-
-    this.parallelogram.display();
-
-    this.popMatrix();
-
-    // Drawing the pink triangle
-    this.pushMatrix();
-
-    this.translate(0, -0.46, 0);
-    this.rotate(-Math.PI/4, 0, 0, 1);
-    this.translate(1, 1, 0);
-
-    this.pinkTriangle.display();
-
-    this.popMatrix();
-
-    // Drawing the purple triangle
-    this.pushMatrix();
-
-    this.translate(-1.5, -3, 0);
-    this.rotate(-3*Math.PI/4, 0, 0, 1);
-
-    this.purpleTriangle.display();
-
-    this.popMatrix();
-
-    // Drawing the red triangle
-    this.pushMatrix();
-
-    this.translate(Math.sqrt(2), -2, 0);
-    this.rotate(-Math.PI/2, 0, 0, 1);
-
-    this.redTriangle.display();
-
-    this.popMatrix();
+    this.tangram.display()
     // ---- END Primitive drawing section
   }
 }
--- tp2/README.md
+++ tp2/README.md
@@ -1,1 +1,18 @@
-# [COPY FROM PREVIOUS TP AND ADJUST]
+# CG 2022/2023
+
+## Group T03G06
+
+## TP 2 Notes
+
+### Exercise 1
+- The tangram part exercise turned out to be relatively straightforward after figuring out how to properly make use of the transformation functions.
+- Keeping in mind that transformations must be written in reverse order can be difficult, but there's an easy work around: write the first transformation and then write each following transformation in the line directly above the previous.
+- Checking the "CFG.js" file in the lib folder revealed that the class *CFGobject* keeps a reference to the scene it's created in, which facilitated the implementation of the *MyTangram* class.
+
+### Exercise 2
+- TODO
+
+### Exercise 3
+- TODO
+
+![Screenshot 1](screenshots/cg-t03g06-tp2-1.png)
