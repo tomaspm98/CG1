ba9ad279d16bed2423e9ad2c10f35fdcb91081be
Marcelo Apolinario <up201603903@fe.up.pt>
2023-03-22 12:28:26 +0000

    TP5, Part 2, Steps 1 & 2

--- tp5/ShaderScene.js
+++ tp5/ShaderScene.js
@@ -71,11 +71,12 @@ export class ShaderScene extends CGFscene {
 		this.appearance.setSpecular(0.0, 0.0, 0.0, 1);
 		this.appearance.setShininess(120);
 
-		this.texture = new CGFtexture(this, "textures/texture.jpg");
+		this.texture = new CGFtexture(this, "textures/waterTex.jpg");
 		this.appearance.setTexture(this.texture);
 		this.appearance.setTextureWrap('REPEAT', 'REPEAT');
 
-		this.texture2 = new CGFtexture(this, "textures/FEUP.jpg");
+		this.texture2 = new CGFtexture(this, "textures/waterMap.jpg");
+
 
 		// shaders initialization
 
@@ -91,6 +92,7 @@ export class ShaderScene extends CGFscene {
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/convolution.frag"),
 			new CGFshader(this.gl, "shaders/ukraine.vert", "shaders/ukraine.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/grayscale.frag"),
+			new CGFshader(this.gl, "shaders/water.vert", "shaders/water.frag"),
 		];
 
 		// additional texture will have to be bound to texture unit 1 later, when using the shader, with "this.texture2.bind(1);"
@@ -98,6 +100,7 @@ export class ShaderScene extends CGFscene {
 		this.testShaders[5].setUniformsValues({ uSampler2: 1 });
 		this.testShaders[6].setUniformsValues({ uSampler2: 1 });
 		this.testShaders[6].setUniformsValues({ timeFactor: 0 });
+		this.testShaders[11].setUniformsValues({ uSampler2: 1 });
 
 
 		// Shaders interface variables
@@ -114,6 +117,7 @@ export class ShaderScene extends CGFscene {
 			'Convolution': 8,
 			'Ukraine': 9,
 			'Grayscale': 10,
+			'Water': 11,
 		};
 
 		// shader code panels references
