03aa123023a16001f2e74b394d4f4e5e43366d2e
Marcelo Guarniero Apolinário <up201603903@up.pt>
2023-05-04 16:46:51 +0100

    Project - Commit for Bird section tag

--- project/MyBird.js
+++ project/MyBird.js
@@ -82,9 +82,9 @@ export class MyBird extends CGFobject {
         this.scene.pushMatrix();
 
         this.scene.translate(this.x, this.y + this.dy, this.z);
-        this.scene.translate(0,5,-0.6);
+        this.scene.translate(0.0, this.y, -0.6);
         this.scene.rotate(this.orientation, 0, 1, 0);
-        this.scene.translate(0,-5,0.6);
+        this.scene.translate(0.0, -this.y, 0.6);
         this.scene.scale(this.scaleFactor, this.scaleFactor, this.scaleFactor);
 
         this.head.display();
